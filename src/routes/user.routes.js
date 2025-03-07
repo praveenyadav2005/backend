2ed1328378d79d46379aabb59463c20ccbaf61e9
praveenyadav2005 <ggpppyadav@gmail.com>
2025-03-07 16:47:32 +0530

    add user account management routes: change password, get current user, and update avatar/cover image

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,9 @@
 import { Router } from "express";
-import { registerUser ,loginUser,logoutUser, refreshAccessToken} from "../controllers/user.controller.js";
+import { registerUser ,loginUser,logoutUser, refreshAccessToken, changePassword,
+  getCurrentUser,
+  updateAccountDetail,
+  updateAvatar,
+  updateCoverImage} from "../controllers/user.controller.js";
 import {upload} from "../middlewares/multer.middleware.js"
 import {verifyJWT} from "../middlewares/auth.middleware.js"
 
@@ -23,7 +27,12 @@ router.route("/login").post(loginUser)
 
 //*Secure routes
 router.route("/logout").post(verifyJWT,logoutUser)
-
 router.route("/refresh-token").post(refreshAccessToken)
+router.route("/password-change").patch(verifyJWT,changePassword)
+router.route("/get-current-user").get(verifyJWT,getCurrentUser)
+router.route("/update-account").patch(verifyJWT,updateAccountDetail)
+router.route("/update-avatar").patch(verifyJWT,upload.single("avatar"),updateAvatar)
+router.route("/update-cover-image").patch(verifyJWT,upload.single("coverImage"),updateCoverImage)
+
 
 export default router
